b250646ebfce5e3ddce17089ab98a4d004e3c388
Michael Ruiz <michael.ruiz@hlioengineering.com>
2022-08-02 13:16:56 -0500

    Fixed small issues building.

--- components/gif-hidder.js
+++ components/gif-hidder.js
@@ -1,6 +1,4 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
-import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 const gifHidder = () => {
 
